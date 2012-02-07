b5518a4c9212f45dc660848dcb779b7566030f70
William Millard <torqueware@gmail.com>
2012-02-07 12:11:39 -0800

    test commit

---
+++ MoombaPlusMobile/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MoombaPlusMobile
+//
+//  Created by  on 2/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
