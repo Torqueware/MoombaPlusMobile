4445a4090a7a1ff8f49a903bb6fc01474c788f00
DJ Mitchell <dj_trumpet@hotmail.com>
2012-03-01 16:26:08 -0800

    MoombaPlusController is useless now

--- MoombaPlusMobile/MoombaPlusController.h
+++ MoombaPlusMobile/MoombaPlusController.h
@@ -2,22 +2,12 @@
 //  MoombaPlusController.h
 //  MoombaPlusMobile
 //
-//  Created by Mitchell, DJ on 2/11/12.
+//  Created by Mitchell, DJ on 3/1/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-#import "IRCController.h"
-#import "RSSController.h"
-#import "RadioController.h"
-
-#define MOOMBA_PLUS_RADIO @"http://s6.voscast.com:7662"
-#define MOOMBA_PLUS_FEED  @"http://moombahplus.com/feed"
 
 @interface MoombaPlusController : NSObject
-
-@property (strong, nonatomic) IRCController     *chatController;
-@property (strong, nonatomic) RSSController     *blogController;
-@property (strong, nonatomic) RadioController   *radioController;
 
 @end
--- MoombaPlusMobile/MoombaPlusController.m
+++ MoombaPlusMobile/MoombaPlusController.m
@@ -2,29 +2,12 @@
 //  MoombaPlusController.m
 //  MoombaPlusMobile
 //
-//  Created by Mitchell, DJ on 2/11/12.
+//  Created by Mitchell, DJ on 3/1/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
 #import "MoombaPlusController.h"
 
 @implementation MoombaPlusController
-
-@synthesize chatController = _chatController;
-@synthesize blogController = _blogController;
-@synthesize radioController = _radioController;
-
-- (id) init {
-    self = [super init];
-   
-    _chatController  = [[IRCController   alloc] init];
-    _blogController  = [[RSSController   alloc] init];
-    _radioController = [[RadioController alloc] init];
-
-   [self.blogController  setURL:[NSURL URLWithString:MOOMBA_PLUS_FEED]];
-   [self.radioController setURL:[NSURL URLWithString:MOOMBA_PLUS_RADIO]];
-    
-    return self;
-}
 
 @end
